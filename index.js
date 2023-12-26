61ff26cc76c0ab9f71126517a733e9ac670d5658
faris-HC <154576474+faris-HC@users.noreply.github.com>
2023-12-26 14:49:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hey, I\'m a Node.js app edited by faris!');
 });
 
-app.listen(8015, '127.0.0.1', () => {
-  console.log(`Server is up on http://127.0.0.1:8015`);
+app.listen(8025, '127.0.0.1', () => {
+  console.log(`Server is up on http://127.0.0.1:8025`);
 });
 
