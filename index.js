9650aeb9a2e34964693e61f6f55bd1dc2688830f
faris-HC <154576474+faris-HC@users.noreply.github.com>
2023-12-26 16:16:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hey, I\'m a Node.js app edited by farisp the final !');
+  res.send('Hey, I\'m a Node.js app edited by farisp the finalz !');
 });
 
 app.listen(8025, '0.0.0.0', () => {
