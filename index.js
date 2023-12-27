d1ba675781db9dc9ee9c25acfeeee8909ebe39c2
faris-HC <154576474+faris-HC@users.noreply.github.com>
2023-12-27 15:36:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hey, I\'m a Node.js app from Jenkins test!');
+  res.send('Hey, I\'m a Node.js app from Jenkins test 2!');
 })
 
 app.listen(8006, '127.0.0.1', () => {
